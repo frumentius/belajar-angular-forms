b334a0c531f6e54f6493a6541f276cd2b6322f32
Frumentius <frumentius_sonic@yahoo.com>
2023-06-06 18:07:49 +0700

    Membuat reactive form.

--- src/app/form-reactive/form-reactive.component.ts
+++ src/app/form-reactive/form-reactive.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-reactive',
   templateUrl: './form-reactive.component.html',
   styleUrls: ['./form-reactive.component.css']
 })
-export class FormReactiveComponent {
+export class FormReactiveComponent implements OnInit {
+  // the '!' sign is to tell typescript that this value will definetly be assigned before use, so it doesn't generate error. Or you can remove the '!' sign and use the value intiation commented in constructor.
+  myForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    // this.myForm = new FormGroup({});
+  }
+
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  submitForm() {
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+      // Perform further actions with the form data
+    }
+  }
+  resetForm() {
+    this.myForm.reset();
+    this.myForm.markAsPristine();
+    this.myForm.markAsUntouched();
+  }
 }
--- src/app/form-reactive/form-reactive.module.ts
+++ src/app/form-reactive/form-reactive.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { FormReactiveRoutingModule } from './form-reactive-routing.module';
 import { FormReactiveComponent } from './form-reactive.component';
@@ -11,7 +12,8 @@ import { FormReactiveComponent } from './form-reactive.component';
   ],
   imports: [
     CommonModule,
-    FormReactiveRoutingModule
+    FormReactiveRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class FormReactiveModule { }
